e8ce365e2fdf9d52cbfe620be6e24a382c310778
cherrabkarim197 <86166827+cherrabkarim197@users.noreply.github.com>
2022-10-12 21:04:21 +0200

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCtgVzK3ARSqW69BaH_Yy7fmlrwB_7aht0",
+    authDomain: "robotweb-895a5.firebaseapp.com",
+    projectId: "robotweb-895a5",
+    storageBucket: "robotweb-895a5.appspot.com",
+    messagingSenderId: "330626742639",
+    appId: "1:330626742639:web:edf14e2d507c20f603509a",
+    measurementId: "G-78HY4HSPW0"
+}
+firebase.initializeApp(firebaseConfig);
+
+const dbRef = firebase.database().ref("lien").once("value",(snap)=>{
+  window.open(snap.val(), '_top');
+});
+
+
+
+
+//npm install -g firebase-tools
