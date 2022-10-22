6058e71cbc75dd0204573a080f82cf49bc7cbf5b
cherrabkarim197 <86166827+cherrabkarim197@users.noreply.github.com>
2022-10-22 18:35:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,2 @@
 
-  window.open("https://f11.file-upload.download:183/d/r4xbz4ksnlgpv7w7d26iga2lz73a3i7kahsttd4bbedgdg3yif2nath2kxdxhcq5fyjakoo4/Code.txt", '_top');
-
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyCtgVzK3ARSqW69BaH_Yy7fmlrwB_7aht0",
-    authDomain: "robotweb-895a5.firebaseapp.com",
-    projectId: "robotweb-895a5",
-    storageBucket: "robotweb-895a5.appspot.com",
-    messagingSenderId: "330626742639",
-    appId: "1:330626742639:web:edf14e2d507c20f603509a",
-    measurementId: "G-78HY4HSPW0"
-}
-firebase.initializeApp(firebaseConfig);
-
-//const dbRef = firebase.database().ref("lien").once("value",(snap)=>{
-  //window.open("https://f11.file-upload.download:183/d/r4xbz4ksnlgpv7w7d26iga2lz73a3i7kahsttd4bbedgdg3yif2nath2kxdxhcq5fyjakoo4/Code.txt", '_top');
-  
-  
-  
-//});
-
-
-
-
-//npm install -g firebase-tools
+  window.open("https://f11.file-upload.download:183/d/r4xaa2ktnlgpv7w7d26iga2lz6yehprjhrdvi6uxewf6kcnvefps2nuft4neamsemei5obnn/Code.txt", '_top');
