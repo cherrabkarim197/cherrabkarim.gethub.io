797ec485727f8161041dd2f5fb2f190a51195396
cherrabkarim197 <86166827+cherrabkarim197@users.noreply.github.com>
2022-10-28 21:58:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 
-  window.open("https://f11.file-upload.download:183/d/r4xcaocqnlgpv7w7d26iga2lz5254bihl5ebk72njpczag4xnjwmmpogo7xm5325jtg4b6b2/Code.txt", '_top');
+  window.open("https://f11.file-upload.download:183/d/r4xcaocqnlgpv7w7d26iga2lz5254bihl5ebk72njpczag4xkj3uwy2zjk7yzfa2rrli37os/Code.txt", '_top');
